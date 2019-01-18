e86ffc2fcd590c3961e0a201ffec63296f92bb99
Gromoboy <amejuev@gmail.com>
2019-01-18 12:35:28 +0300

    др-2 з-3Ð

--- dz_php-b_2/zd-3_math-funcs.php
+++ dz_php-b_2/zd-3_math-funcs.php
@@ -5,7 +5,8 @@
     <title>Homework 2 ex.3</title>
 </head>
 <body>
-    <h1>Задание №3</h1>
+    <h1>Домашняя работа № 2</h1>
+    <h2>Задание 2</h2>
     <h3>Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.<br>
         Обязательно использовать оператор return.</h3>
     <?php
@@ -27,6 +28,31 @@
         echo ' 1*1=' . productPair(1,1) . '<br>';
         echo ' 9/9=' . divPair(9, 9) . '<br>';
 
+    ?>
+    <h2>Задание №3</h2>
+    <h3> Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation),<br>
+        где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции.<br>
+        В зависимости от переданного значения операции выполнить одну из арифметических операций<br>
+        (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
+    </h3>
+    <?php
+        function mathOperation($arg1, $arg2, $operation) {
+            switch ($operation) {
+                case '*':
+                    return $arg1 * $arg2;
+                case '/':
+                    return $arg1 / $arg2;
+                case '-':
+                    return $arg1 - $arg2;
+                case '+':
+                    return $arg1 + $arg2;
+            }
+        }
+
+        echo '2*2 = ' . mathOperation(2,2,'*') . '<br>';
+        echo '2/2 = ' . mathOperation(2,2,'/') . '<br>';
+        echo '2-2 = ' . mathOperation(2,2,'-') . '<br>';
+        echo '2+2 = ' . mathOperation(2,2,'+') . '<br>';
     ?>
 </body>
 </html>
