5291ca49faf86c90954ac003a40493a5450d36bb
Gromoboy <amejuev@gmail.com>
2019-01-18 13:40:22 +0300

    6 задание Домашней работы №2

--- dz_php-b_2/zd-6_recursion.php
+++ dz_php-b_2/zd-6_recursion.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 18.01.2019
- * Time: 13:15
- */
+
+    function power($val, $pow) {
+        if ($pow === 1) return $val;
+        return $val * power($val, --$pow);
+    }
+    $first = power(1, 2);
+    $second = power(2, 2);
+    $third = power(3,3);
+echo <<<html
+<html>
+<head>
+<meta charset="utf-8">
+<title>Дз-2 зд-6*</title>
+</head>
+<body>
+<h1>Задание №6*</h1>
+<h3>*С помощью рекурсии организовать функцию возведения числа в степень.<br>
+ Формат: function power($val, $pow), где $val – заданное число, $pow – степень.</h3>
+ <p>1<sup>2</sup>= $first</p>
+     <p>2<sup>2</sup>= $second</p>
+     <p>3<sup>3</sup>= $third</p>
+</body>
+</html>
+html;
+
